8d32e456acfc11dfe31ad471ecf1d711493daa76
Manon3005 <manon.b2003@gmail.com>
2025-10-27 09:22:43 -0300

    ✨ init product page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -46,7 +46,12 @@ const routes: Routes = [
     path: '404',
     loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorPageModule)
   },
-  { path: '**', redirectTo: '404' } 
+  { path: '**', redirectTo: '404' },   
+  {
+    path: 'product',
+    loadChildren: () => import('./pages/product/product.module').then( m => m.ProductPageModule)
+  }
+
 ];
 
 @NgModule({
