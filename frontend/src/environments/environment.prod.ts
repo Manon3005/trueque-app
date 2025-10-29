fe61a777b4719f14c6bdf0d28e274b8c649e0eb1
nelsoneitor12 <nelson.fuentes.l@mail.pucv.cl>
2025-10-29 15:23:51 -0300

    enviroment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-// src/environments/environment.ts
+
 export const environment = {
   production: false,
-  // ‼️ ¡Apunta esto a tu backend!
-  // Lo deduzco de tu .env (PORT=3000)
+
   apiUrl: 'http://localhost:3000/api' 
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,11 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// src/environments/environment.ts
+
 export const environment = {
   production: false,
-  // ‼️ ¡Apunta esto a tu backend!
-  // Lo deduzco de tu .env (PORT=3000)
+  
   apiUrl: 'http://localhost:3000/api' 
 };
 /*
